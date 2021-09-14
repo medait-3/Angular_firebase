28b2c0cf4cea1360ba3a9fde9ab0162327c1b7e1
meait-3 <tchikh3@@gmail.com>
2021-09-14 15:09:36 +0100

    first commit

---
+++ src/app/post.service.ts
@@ -0,0 +1,46 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Post } from './post.model';
+
+@Injectable({ providedIn: 'root' })
+export class PostService {
+  listChangedEvent: EventEmitter<Post[]> = new EventEmitter();
+  listOfPosts: Post[] = [
+
+  ];
+
+  // facility 1
+  getPosts() {
+    return this.listOfPosts;
+  }
+
+  // facilty 2
+  deletePost(index: number) {
+    this.listOfPosts.splice(index, 1);
+  }
+
+  // Facility 3
+  addPost(post: Post) {
+    this.listOfPosts.push(post);
+  }
+
+  // facility 4
+  updatePost(index: number, post: Post) {
+    this.listOfPosts[index] = post;
+  }
+
+  // facility 5
+  getPost(index: number) {
+    return this.listOfPosts[index];
+  }
+
+  // facility 6
+  likePost(index: number) {
+    this.listOfPosts[index].numberOfLikes += 1;
+  }
+
+  // facility 7
+  setPosts(listOfPosts: Post[]) {
+    this.listOfPosts = listOfPosts;
+    this.listChangedEvent.emit(listOfPosts);
+  }
+}
